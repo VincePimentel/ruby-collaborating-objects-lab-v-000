7e58e317c5291a8c99aefb1d09718475fc1627b4
Vince Andrew Pimentel <contact@vincep.me>
2018-10-24 19:43:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,15 +6,15 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = filename.split(" - ")[1]
+    songx = filename.split(" - ")[1]
     artist = filename.split(" - ")[0]
-    song = self.new(song)
+    song = self.new(songx)
     song.artist_name = artist
     song
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    Artist.add_song(self)
+    artist.add_song(self)
   end
 end
