550acef98a02b1e435f18db51f6a4f91a9d7e0a8
Vince Andrew Pimentel <contact@vincep.me>
2018-10-24 18:03:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,10 @@ class Song
     @name = name
   end
 
+  def artist_name
+  self.artist ? self.artist.name : nil
+end
+
   def self.new_by_filename(filename)
     parsed_song = filename.split(" - ")[1]
     parsed_artist = filename.split(" - ")[0]
