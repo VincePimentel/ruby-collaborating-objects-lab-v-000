29af743e09aedf053fe46a2cf22c46d0d7956a47
Vince Andrew Pimentel <contact@vincep.me>
2018-10-24 19:03:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,8 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-  end
-
-  def artist=(artist)
-    @artist = artist
-  end
-
-  def artist
-    @artist
   end
 
   def self.new_by_filename(filename)
